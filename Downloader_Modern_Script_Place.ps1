b976195c1b5828c0cdd873481f5742436a569185
AlexODC <157601729+AlexODC@users.noreply.github.com>
2024-02-12 11:40:19 +0100

    Update and rename Download_script_deploy.ps1 to Downloader_Modern_Script_Place.ps1

--- Downloader_Modern_Script_Place.ps1
+++ Downloader_Modern_Script_Place.ps1
@@ -1,6 +1,6 @@
-﻿$url = "https://raw.githubusercontent.com/AlexODC/script_deployISO/main/Script_Deploy_d%C3%A9ploy%C3%A9.ps1" # URL du script de déploiement
+$url = 'https://raw.githubusercontent.com/AlexODC/script_deployISO/main/Script_Deploy_d%C3%A9ploy%C3%A9.ps1' # URL du script de déploiement
 $desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
-$output = Join-Path $desktopPath "script_deployISO.ps1"
+$output = Join-Path $desktopPath "Modern_Script_Place.ps1"
 
 # Télécharger le contenu du script en mémoire
 $response = Invoke-WebRequest -Uri $url
@@ -11,6 +11,6 @@ $response.Content | Out-File -FilePath $output -Encoding UTF8
 # Exécuter le script téléchargé
 Start-Process PowerShell -ArgumentList "-ExecutionPolicy Bypass -File `"$output`""
 
-# Supprimer le fichier téléchargé (Si nécessaire. Notez que la ligne actuelle semble supprimer un autre script. Assurez-vous que le chemin est correct.)
+# Supprimer le fichier téléchargé
 $cheminDuFichier = Join-Path $desktopPath "Download_script_deploy.ps1"
 Remove-Item -Path $cheminDuFichier -Force
