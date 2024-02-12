6967c2b5762327cc9ab47d31e9639fd94baa57b9
AlexODC <157601729+AlexODC@users.noreply.github.com>
2024-02-12 15:10:21 +0100

    Update Downloader_Modern_Script_Place.ps1
    Modification de nom et suppression du force qui fait une erreur de chemin

--- Downloader_Modern_Script_Place.ps1
+++ Downloader_Modern_Script_Place.ps1
@@ -1,4 +1,4 @@
-$url = 'https://raw.githubusercontent.com/AlexODC/script_deployISO/main/Script_Deploy_d%C3%A9ploy%C3%A9.ps1' # URL du script de déploiement
+$url = 'https://raw.githubusercontent.com/AlexODC/script_deployISO/main/Modern_Script_Place.ps1' # URL du script de déploiement
 $desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
 $output = Join-Path $desktopPath "Modern_Script_Place.ps1"
 
@@ -8,9 +8,7 @@ $response = Invoke-WebRequest -Uri $url
 # Écrire le contenu en spécifiant l'encodage UTF-8
 $response.Content | Out-File -FilePath $output -Encoding UTF8
 
-# Exécuter le script téléchargé
-Start-Process PowerShell -ArgumentList "-ExecutionPolicy Bypass -File `"$output`""
+Start-Process PowerShell -ArgumentList " -ExecutionPolicy Bypass -File `"$output`""
 
-# Supprimer le fichier téléchargé
-$cheminDuFichier = Join-Path $desktopPath "Modern_Script_Place.ps1"
-Remove-Item -Path $cheminDuFichier -Force
+$cheminDuFichier = Join-Path $desktopPath "Downloader_Modern_Script_Place.ps1"
+Remove-Item -Path $cheminDuFichier
