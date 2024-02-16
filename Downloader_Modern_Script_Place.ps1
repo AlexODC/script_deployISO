9d9f20393d0fca1c3887dde48d1de8f22f49e302
AlexODC <157601729+AlexODC@users.noreply.github.com>
2024-02-16 11:23:54 +0100

    Update Downloader_Modern_Script_Place.ps1

--- Downloader_Modern_Script_Place.ps1
+++ Downloader_Modern_Script_Place.ps1
@@ -5,10 +5,17 @@ $output = Join-Path $desktopPath "Modern_Script_Place.ps1"
 # Télécharger le contenu du script en mémoire
 $response = Invoke-WebRequest -Uri $url
 
-# Écrire le contenu en spécifiant l'encodage UTF-8
-$response.Content | Out-File -FilePath $output -Encoding UTF8
+# Vérifier si le contenu téléchargé n'est pas vide avant de l'écrire dans un fichier
+if ($response.Content.Length -gt 0) {
+    # Écrire le contenu en spécifiant l'encodage UTF-8
+    $response.Content | Out-File -FilePath $output -Encoding UTF8
 
-Start-Process PowerShell -ArgumentList " -ExecutionPolicy Bypass -File `"$output`""
+    Start-Process PowerShell -ArgumentList " -ExecutionPolicy Bypass -File `"$output`""
 
-$cheminDuFichier = Join-Path $desktopPath "Downloader_Modern_Script_Place.ps1"
-Remove-Item -Path $cheminDuFichier
+    # Supprimer le script de téléchargement seulement si le fichier téléchargé n'est pas vide
+    $cheminDuFichier = Join-Path $desktopPath "Downloader_Modern_Script_Place.ps1"
+    Remove-Item -Path $cheminDuFichier
+} else {
+    # Si le contenu est vide, afficher un message d'erreur ou prendre une autre action
+    Write-Host "Le téléchargement du script a échoué ou le script est vide."
+}
